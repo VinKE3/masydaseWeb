667b660bc9a4bb676af45e81d249b7f3c0792f04
Kevin Muñoz <harquin09@gmail.com>
2024-01-03 17:45:42 -0500

    benefit

---
+++ components/Benefits/Benefits.data.ts
@@ -0,0 +1,22 @@
+import { BenefitsData } from "./Benefits.types";
+
+export const dataBenefits: BenefitsData = [
+  {
+    id: 1,
+    title: "Seguridad y Confiabilidad",
+    description:
+      "Nos preocupamos por la seguridad de tu información, espor eso que nuestro sistema se respalda de manera automática.",
+  },
+  {
+    id: 2,
+    title: "Soporte y Asesoría Técnica",
+    description:
+      "Contamos con profesionales certificados con el fin de dar apoyo, podemos conectarnos a tu equipo de forma remota mediante TeamViewer.",
+  },
+  {
+    id: 3,
+    title: "Plataforma Web de Facturas Electrónicas",
+    description:
+      "MASYDASE cuenta con una plataforma web en donde elcliente podrá consultar y descargar sus facturas electrónicas.",
+  },
+];
--- components/Benefits/Benefits.tsx
+++ components/Benefits/Benefits.tsx
@@ -5,6 +5,7 @@ import { MotionTransition } from "../MotionTransition";
 import { Reveal } from "../Reveal";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { dataBenefits } from "./Benefits.data";
 
 export default function Benefits() {
   const { theme } = useTheme();
@@ -48,45 +49,24 @@ export default function Benefits() {
               </div>
               <MotionTransition>
                 <div className="grid grid-cols-1 gap-2 mt-12 sm:grid-cols-3 lg:mt-20">
-                  <div className="transition-all duration-1000 dark:bg-cyan-600 dark:hover:bg-cyan-800 bg-white hover:bg-azul  hover:shadow-xl m-2 p-4 relative z-40 group  ">
-                    <div className="absolute dark:bg-white dark:group-hover:bg-cyan-600 bg-azul/80 top-0 left-0 w-24 h-1 z-30  transition-all duration-200 group-hover:bg-white group-hover:w-1/2  "></div>
-                    <div className="py-2 px-9 relative  ">
-                      <h3 className="mt-8 text-lg font-semibold text-grayDark group-hover:text-white dark:text-white">
-                        Seguridad y Confiabilidad
-                      </h3>
-                      <p className="mt-4 text-base text-grayDark dark:text-white group-hover:text-white">
-                        Nos preocupamos por la seguridad de tu información, es
-                        por eso que nuestro sistema se respalda de manera
-                        automática.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="transition-all duration-1000 dark:bg-cyan-600 dark:hover:bg-cyan-800 bg-white hover:bg-azul  hover:shadow-xl m-2 p-4 relative z-40 group  ">
-                    <div className="absolute dark:bg-white dark:group-hover:bg-cyan-600 bg-azul/80 top-0 left-0 w-24 h-1 z-30  transition-all duration-200 group-hover:bg-white group-hover:w-1/2  "></div>
-                    <div className="py-2 px-9 relative  ">
-                      <h3 className="mt-8 text-lg font-semibold text-grayDark group-hover:text-white dark:text-white">
-                        Soporte y Asesoría Técnica
-                      </h3>
-                      <p className="mt-4 text-base text-grayDark dark:text-white group-hover:text-white">
-                        Contamos con profesionales certificados con el fin de
-                        dar apoyo, podemos conectarnos a tu equipo de forma
-                        remota mediante TeamViewer.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="transition-all duration-1000 dark:bg-cyan-600 dark:hover:bg-cyan-800 bg-white hover:bg-azul  hover:shadow-xl m-2 p-4 relative z-40 group  ">
-                    <div className="absolute dark:bg-white dark:group-hover:bg-cyan-600 bg-azul/80 top-0 left-0 w-24 h-1 z-30  transition-all duration-200 group-hover:bg-white group-hover:w-1/2  "></div>
-                    <div className="py-2 px-9 relative  ">
-                      <h3 className="mt-8 text-lg font-semibold text-grayDark group-hover:text-white dark:text-white">
-                        Plataforma Web de Facturas Electrónicas
-                      </h3>
-                      <p className="mt-4 text-base text-grayDark dark:text-white group-hover:text-white">
-                        MASYDASE cuenta con una plataforma web en donde el
-                        cliente podrá consultar y descargar sus facturas
-                        electrónicas.
-                      </p>
-                    </div>
-                  </div>
+                  {dataBenefits.map((item) => {
+                    return (
+                      <div
+                        key={item.id}
+                        className="transition-all duration-1000 dark:bg-cyan-600 dark:hover:bg-cyan-800 bg-white hover:bg-azul  hover:shadow-xl m-2 p-4 relative z-40 group  "
+                      >
+                        <div className="absolute dark:bg-white dark:group-hover:bg-cyan-600 bg-azul/80 top-0 left-0 w-24 h-1 z-30  transition-all duration-200 group-hover:bg-white group-hover:w-1/2  "></div>
+                        <div className="py-2 px-9 relative  ">
+                          <h3 className="mt-8 text-lg font-semibold text-grayDark group-hover:text-white dark:text-white">
+                            {item.title}
+                          </h3>
+                          <p className="mt-4 text-base text-grayDark dark:text-white group-hover:text-white">
+                            {item.description}
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  })}
                 </div>
               </MotionTransition>
             </div>
