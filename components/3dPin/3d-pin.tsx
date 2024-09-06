a0ee63c1582c599e44f61e1e12dd12c6eb7223be
Kevin Muñoz <harquin09@gmail.com>
2024-09-06 12:15:18 -0500

    beneficios

--- components/3dPin/3d-pin.tsx
+++ components/3dPin/3d-pin.tsx
@@ -22,7 +22,7 @@ export const PinContainer = ({
   );
 
   const onMouseEnter = () => {
-    setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
+    setTransform("translate(-50%,-50%) rotateX(30deg) scale(0.8)");
   };
   const onMouseLeave = () => {
     setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
@@ -36,7 +36,7 @@ export const PinContainer = ({
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      href={href || "/"}
+      href={href || "#"}
     >
       <div
         style={{
@@ -49,9 +49,9 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
-          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] dark:bg-cyan-600 dark:hover:bg-cyan-800 bg-white hover:bg-zinc-100 border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
         >
-          <div className={cn(" relative z-50 ", className)}>{children}</div>
+          <div className={cn(" relative z-50", className)}>{children}</div>
         </div>
       </div>
       <PinPerspective title={title} href={href} />
--- components/Benefits/Benefits.tsx
+++ components/Benefits/Benefits.tsx
@@ -45,22 +45,26 @@ export default function Benefits() {
                 </div>
               </div>
               <MotionTransition>
-                <div className="grid grid-cols-1 gap-2 mt-12 sm:grid-cols-3 lg:mt-20">
-                  {dataBenefits.map((item) => {
+                <div className="grid grid-cols-1 gap-2 mt-12  md:grid-cols-2 lg:grid-cols-3 lg:mt-20">
+                  {dataBenefits.map(({ id, title, description }) => {
                     return (
-                      <div
-                        key={item.id}
-                        className="transition-all duration-1000 dark:bg-cyan-600 dark:hover:bg-cyan-800 bg-white hover:bg-azul  hover:shadow-xl m-2 p-4 relative z-40 group  "
-                      >
-                        <div className="absolute dark:bg-white dark:group-hover:bg-cyan-600 bg-azul/80 top-0 left-0 w-24 h-1 z-30  transition-all duration-200 group-hover:bg-white group-hover:w-1/2  "></div>
-                        <div className="py-2 px-9 relative  ">
-                          <h3 className="mt-8 text-lg font-semibold text-grayDark group-hover:text-white dark:text-white">
-                            {item.title}
-                          </h3>
-                          <p className="mt-4 text-base text-grayDark dark:text-white group-hover:text-white">
-                            {item.description}
-                          </p>
-                        </div>
+                      <div className="h-[30rem] w-full flex items-center justify-center ">
+                        <PinContainer title="Ver Planes" href="#planes">
+                          <div
+                            key={id}
+                            className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[27rem]"
+                          >
+                            <h3 className="max-w-xs !pb-2 !m-0 font-bold text-lg  text-grayDark dark:text-white">
+                              {title}
+                            </h3>
+                            <div className="text-base !m-0 !p-0 font-normal">
+                              <span className="text-base text-grayDark dark:text-white">
+                                {description}
+                              </span>
+                            </div>
+                            <div className="flex flex-1 w-full h-[2px] rounded-lg mt-4 transition-all duration-1000 dark:bg-blueRadial from-primary to-azul bg-gradient-to-r  hover:shadow-xl" />
+                          </div>
+                        </PinContainer>
                       </div>
                     );
                   })}
@@ -68,24 +72,6 @@ export default function Benefits() {
               </MotionTransition>
             </div>
           </div>
-        </div>
-        <div className="h-[30rem] w-full flex items-center justify-center ">
-          <PinContainer title="Ver Planes" href="#planes">
-            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-              <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                Seguridad y Confiabilidad
-              </h3>
-              <div className="text-base !m-0 !p-0 font-normal">
-                <span className="text-slate-500 ">
-                  Nos preocupamos por la seguridad de tu información, espor eso
-                  que nuestro sistema se respalda de manera automática. Nos
-                  preocupamos por la seguridad de tu información, espor eso que
-                  nuestro sistema se respalda de manera automática.
-                </span>
-              </div>
-              <div className="flex flex-1 w-full h-[10px] rounded-lg mt-4 transition-all duration-1000 dark:bg-cyan-600 dark:hover:bg-cyan-800 bg-white hover:bg-azul  hover:shadow-xl" />
-            </div>
-          </PinContainer>
         </div>
       </div>
     </>
