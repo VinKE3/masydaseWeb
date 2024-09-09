ddb9bd9b2998883e5e74f9cd67c76e0da8000cef
Kevin Muñoz <harquin09@gmail.com>
2024-09-09 12:47:02 -0500

    cards

--- components/ChooseYourCards/ChooseYourCards.tsx
+++ components/ChooseYourCards/ChooseYourCards.tsx
@@ -12,17 +12,72 @@ import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { BackgroundRadialLeft } from "../BackgroundRadialLeft";
+import { StickyScroll } from "../StickyScrollReveal/sticky-scroll-reveal";
 
 export function ChooseYourCards() {
   const { theme } = useTheme();
   const isDark = theme === "dark";
   const [hasMounted, setHasMounted] = useState(false);
-  const pagination = {
-    clickable: true,
-    renderBullet: function (index: number, className: string) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
+  const content = [
+    {
+      title: "E-MasyComercial",
+      description:
+        "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      content: (
+        <Image
+          src="/assets/card1.png"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="E-MasyComercial demo"
+        />
+      ),
     },
-  };
+    {
+      title: "Real time changes",
+      description:
+        "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+      content: (
+        <div className="h-full w-full  flex items-center justify-center text-white">
+          <Image
+            src="/assets/card2.png"
+            width={300}
+            height={300}
+            className="h-full w-full object-cover"
+            alt="E-MasyComercial demo"
+          />
+        </div>
+      ),
+    },
+    {
+      title: "Version control",
+      description:
+        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      content: (
+        <Image
+          src="/assets/card3.png"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="E-MasyComercial demo"
+        />
+      ),
+    },
+    {
+      title: "Running out of content",
+      description:
+        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      content: (
+        <Image
+          src="/assets/card4.png"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="E-MasyComercial demo"
+        />
+      ),
+    },
+  ];
   useEffect(() => {
     setHasMounted(true);
   }, []);
@@ -63,6 +118,9 @@ export function ChooseYourCards() {
             </Swiper>
           </MotionTransition>
         </div>
+      </div>
+      <div className="max-w-7xl mx-auto p-1">
+        <StickyScroll content={content} />
       </div>
     </div>
   );
